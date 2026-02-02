86985850acc7a5b1f5c1ea5f589c407ef08a4366
karthi-raj <karthirajm7@gmail.com>
2026-02-02 19:04:18 +0530

    Time: 8 ms (10.28%) | Memory: 46.6 MB (82.73%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode current = head;
+        
+        while (current != null) {
+            if (visited.contains(current)) {
+                return true;
+            }
+            visited.add(current);
+            current = current.next;
+        }
+        
+        return false;
+    }
+}
